4acdb90f286ed072081fc05876865b7bbef45115
DenisSemko <dench327@gmail.com>
2020-08-01 23:39:27 +0300

    Add subscribedtocustomer field and MembershipType class

--- AudMovie/Models/Customer.cs
+++ AudMovie/Models/Customer.cs
@@ -9,6 +9,9 @@ namespace AudMovie.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public bool IsSubscribedToNewsLetter { get; set; }
+		public MembershipType MembershipType { get; set; }
+		public int MembershipTypeId { get; set; }
 
 	}
 }
